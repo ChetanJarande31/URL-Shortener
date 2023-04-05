cd43267beb4850b43570b22dac17aef8aa640350
Ayush Chothe <ayushmchothe@gmail.com>
2023-04-06 00:35:10 +0530

    Revert

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, requests
 from instaloader import Instaloader, Profile
 from pydantic import BaseModel
 
@@ -36,11 +36,6 @@ async def demo_get_path_id(path_id: int):
 @app.get("/bot/get-insta-profile/{profile_id}")
 async def get_profile_url(profile_id: str):
     response = {
-        "url": Profile.from_username(
-            Instaloader(
-                user_agent="User-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 12_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 Instagram 105.0.0.11.118 (iPhone11,8; iOS 12_3_1; en_US; en-US; scale=2.00; 828x1792; 165586599)"
-            ).context,
-            profile_id,
-        ).profile_pic_url
+        "url": Profile.from_username(Instaloader().context, profile_id).profile_pic_url
     }
     return response
