0140ab81f2a20299065f1a0af1a2e858187598d8
Ayush Chothe <ayushmchothe@gmail.com>
2023-04-06 00:07:53 +0530

    User Agent Added

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
+from instaloader import Instaloader, Profile
 from pydantic import BaseModel
-from instaloader import Profile, Instaloader
 
 app = FastAPI()
+
 
 class Msg(BaseModel):
     msg: str
@@ -15,7 +16,9 @@ async def root():
 
 @app.get("/path")
 async def demo_get():
-    return {"message": "This is /path endpoint, use a post request to transform the text to uppercase"}
+    return {
+        "message": "This is /path endpoint, use a post request to transform the text to uppercase"
+    }
 
 
 @app.post("/path")
@@ -25,10 +28,19 @@ async def demo_post(inp: Msg):
 
 @app.get("/path/{path_id}")
 async def demo_get_path_id(path_id: int):
-    return {"message": f"This is /path/{path_id} endpoint, use post request to retrieve result"}
+    return {
+        "message": f"This is /path/{path_id} endpoint, use post request to retrieve result"
+    }
 
 
 @app.get("/bot/get-insta-profile/{profile_id}")
 async def get_profile_url(profile_id: str):
-    response = {"url" : Profile.from_username(Instaloader().context, profile_id).profile_pic_url}
+    response = {
+        "url": Profile.from_username(
+            Instaloader(
+                user_agent="User-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 12_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 Instagram 105.0.0.11.118 (iPhone11,8; iOS 12_3_1; en_US; en-US; scale=2.00; 828x1792; 165586599)"
+            ).context,
+            profile_id,
+        ).profile_pic_url
+    }
     return response
